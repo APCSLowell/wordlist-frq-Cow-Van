2819020685a3cd8b73cb44c4a8c7053a5d00d938
Calvin <calvinlu@live.com>
2024-02-16 11:15:21 -0800

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,39 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+        int a = 0;
+    for (String b : myList) {
+      if (b.length() == len) {
+        a++;
+      }
+    }
+
+    return a;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
+    ArrayList<String> a = new ArrayList<>();
+    for (String s : myList) {
+      if (s.length() != len) {
+        a.add(s);
+      }
+    }
 
+    myList = a;
   }
 }
+/*
+  .--.            .--.
+ ( (`\\."--``--".//`) )
+  '-.   __   __    .-'
+   /   /__\ /__\   \
+  |    \ 0/ \ 0/    |
+  \     `/   \`     /
+   `-.  /-"""-\  .-`
+     /  '.___.'  \
+     \     I     /
+      `;--'`'--;`
+jgs     '.___.'
+*/
